2e86fd6c309b36b2a68236e10873be2f7dec78b6
flyingsquirrel321 <165931847+flyingsquirrel321@users.noreply.github.com>
2024-08-17 12:54:14 +0300

    Update Scikit-learn_17.08.TW.py

--- Python Teamworks/Scikit-learn_17.08.TW.py
+++ Python Teamworks/Scikit-learn_17.08.TW.py
@@ -1,1 +1,12 @@
+### Teamwork:
+Task: This is a complicated topic, teamwork is to look at scikit-learn library together. Suggested questions to discuss:
+1. What is an error rate?
+2. Where you could use other machine-learning models?
+3. What is the difference between supervised and unsupervised training?
+4. How to import different models from the scikit-learn package?
+5. How can you evaluate the performance of a machine learning model in scikit-learn?
+6. What metrics are commonly used for evaluation?
+7. What is model overfitting, and how can it be prevented?
 
+Linear regression modesls: https://scikit-learn.org/stable/modules/linear_model.html
+The overall documentation: https://scikit-learn.org/stable
